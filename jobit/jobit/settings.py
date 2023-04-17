ebecf7f95dcbc035ef65af4f4e2ca2c2a513bdc4
yulisuliman <yulisuliman1234@gmail.com>
2023-04-17 20:54:09 +0300

    add users appand about

--- jobit/jobit/settings.py
+++ jobit/jobit/settings.py
@@ -128,7 +128,7 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_REDIRECT_URL = 'feed-home'
 LOGIN_URL = 'login'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- jobit/users/forms.py
+++ jobit/users/forms.py
@@ -1,25 +1,36 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 GENDER_CHOICES = (
-    (0, 'male'),
-    (1, 'female'),
-    (2, 'not specified'),
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Other'),
 )
 
-
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
-    profession = forms.CharField(max_length=100, required=False)
-    address = forms.CharField(max_length=50, required=False)
+    date_of_birth = forms.DateField(required=True)
     gender = forms.ChoiceField(choices=GENDER_CHOICES)
     date_of_birth = forms.DateField()
-
+    email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name','username', 'email', 'gender', 'date_of_birth', 'profession', 'address', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'date_of_birth', 'gender', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'profession', 'address', 'image']
