bb3c346c1043887c55d075e36657ef245d02e4c8
Saar Orus <saar4848@gmail.com>
2023-05-06 14:09:39 +0300

    adding files

--- jobit/users/models.py
+++ jobit/users/models.py
@@ -16,6 +16,7 @@ GENDER_CHOICES = (
     ('O', 'Other'),
 )
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
